aafae7f6b90030497fe0f6cd9d2e6704b8773a58
Danish Rasheed <danirasheed60.com>
2025-12-20 09:43:02 +0000

    small change

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
-
-import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
-import { ButtonModule } from "primeng/button";
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-    selector:"app-navbar",
-    templateUrl:"./navbar.component.html",
-    styleUrls:["./navbar.component.css"],
-    imports: [CommonModule, ButtonModule, RouterLink, RouterLinkActive]
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+  imports: [CommonModule, ButtonModule, RouterLink, RouterLinkActive],
 })
-export class NavbarComponent{
+export class NavbarComponent {
+  constructor(private router: Router) {}
 
+  goToHome() {
+    this.router.navigate(['/']);
+  }
 }
