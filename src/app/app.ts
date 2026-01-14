174aa05d6b121dd47390940a476790165f450a63
Danish Rasheed <danirasheed60.com>
2026-01-14 21:13:56 +0000

    some changes for online

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
-import { FooterComponent } from './components/footer/footer.compoenet';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
